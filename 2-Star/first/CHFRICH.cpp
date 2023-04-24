4710a11e9a0ea388398c0560edec684c52d1fd96
Manish N <manish29na@gmail.com>
2023-04-24 21:26:16 +0530

    Richie Rich

---
+++ 2-Star/first/CHFRICH.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d,r;
+        cin>>a>>b>>c;
+        d=b-a;
+        r=d/c;
+        cout<<r<<endl;
+    }
+	return 0;
+}
