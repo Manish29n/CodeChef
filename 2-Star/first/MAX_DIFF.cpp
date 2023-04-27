6efb79f10b15e8ba7827af2dcad0704a1a72216c
Manish N <manish29na@gmail.com>
2023-04-27 20:51:32 +0530

    The Two Dishes

---
+++ 2-Star/first/MAX_DIFF.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,s;
+        cin>>n>>s;
+        if(s<n)
+            cout<<s<<endl;
+        else
+            cout<<2*n-s<<endl;
+    }
+	return 0;
+}
