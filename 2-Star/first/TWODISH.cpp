6eeb87ec3afa6dfc388e33f88cbb4b839713c04e
Manish N <manish29na@gmail.com>
2023-04-28 21:47:23 +0530

    Two Dishes

---
+++ 2-Star/first/TWODISH.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b,c;
+        cin>>n>>a>>b>>c;
+        cout<<((b>=n && a+c >=n)?"YES":"NO")<<endl;
+    }
+	return 0;
+}
