adfaa23bdd80431afc1ea4ca5083bcc948056129
Manish N <manish29na@gmail.com>
2023-05-02 22:33:50 +0530

    Distinct Pair Sums

---
+++ 2-Star/first/MANYSUMS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        cout<<1+(b-a)*2<<endl;
+    }
+	return 0;
+}
