05acc9c5367b62b37ed215748d58fdd2262cb714
Manish N <manish29na@gmail.com>
2023-05-03 19:15:32 +0530

    Tom And Jerry 1

---
+++ 2-Star/first/TANDJ1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--)
+{int a,b,c,d,k;
+cin>>a>>b>>c>>d>>k;
+int s=abs(d-b)+abs(c-a);
+
+if (k>=s && (k-s)%2==0) cout<<"Yes"<<endl;
+else cout<<"No"<<endl;
+
+    
+}
+	return 0;
+}
